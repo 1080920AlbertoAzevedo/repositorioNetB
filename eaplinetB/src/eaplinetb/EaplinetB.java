78dc1b85cdd805e91103d65fb49565a314e8a47c
Carlos <Carlos@CarlosTavares-P>
2013-03-13 16:43:26 +0000

    TELEPORTING POWAW

--- eaplinetB/src/eaplinetb/EaplinetB.java
+++ eaplinetB/src/eaplinetb/EaplinetB.java
@@ -9,18 +9,8 @@ package eaplinetb;
  * @author alberto
  */
 public class EaplinetB {
-
-    public void pausa(millis){
-        
-        var date = new Date();
-        var curDate = null;
-        do { curDate = new Date(); }
-        while(curDate-date < millis);
-    }
     public static void main(String[] args) {
-        for(int i=0;i<7;i++){
-           setTimeout(); 
-        }
+       System.out.println("No error.");
     }
     
 }
